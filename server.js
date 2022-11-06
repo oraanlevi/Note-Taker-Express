9b0c6c29202027d599cb69607771a2d3d2fa0c8d
Oraan Levi <oraanlevi1@gmail.com>
2022-11-06 14:27:19 -0800

    final changes to github

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const notes = require('./db/db.json');
+const uuid = require('uuid');
+const {  DH_CHECK_P_NOT_SAFE_PRIME } = require('constants');
+const exp = require('constants');
+
+const app = express();
+var PORT = process.env.PORT || 3017;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('./public'));
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "./db/db.json"))
+});
+
+app.post("/api/notes", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const newNotes = req.body;
+    newNotes.id = uuid.v4();
+    notes.push(newNotes);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes))
+    res.json(notes);
+
+}); 
+
+app.delete("/api/notes/:id", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const delNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
+    fs.writeFileSync('./db/db.json', JSON.stringify(delNote));
+    res.json(delNote);
+
+});
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+
+});
+
+app.listen(PORT, function () {
+    console.log("APP listening on PORT: + PORT")
+});
+
